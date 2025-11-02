04b039e2f3e1445c3a574d49c102a168cc0df5e4
arash-yarahmadi <arashyarahmadi90@yahoo.com>
2025-11-02 18:23:38 +0330

    feat:implement refreshToken and accessToken

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import Router from "router/Router";
+import defaultOptions from "config/reactQuery";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient({ defaultOptions });
+
   return (
-    <BrowserRouter>
-      <Router />
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        toastOptions={{
-          duration: 5000,
-        }}
-      />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{
+            duration: 5000,
+          }}
+        />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,11 +1,44 @@
 import axios from "axios";
-
+import { getNewTokens } from "src/services/token";
+import { getCookie, SetCookie } from "src/utils/cookie";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
-    headers:{
-        "Content-Type": "application/json"
-    }
-})
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export default api
+api.interceptors.request.use(
+  (request) => {
+    const accessToken = getCookie("accessToken");
+    console.log("AccessToken:", accessToken);
+    if (accessToken) {
+      request.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const res = await getNewTokens();
+      if (!res?.response) return;
+
+      SetCookie(res.response.data);
+
+      return api(originalRequest);
+    }
+  }
+);
+
+export default api;
